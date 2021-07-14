fc25ac5ea81a14f87da0c57280134b4e358b5893
tomasjindrich <ahoj@tomasjindrich.cz>
2021-07-14 11:56:11 +0200

    rozkopávky

---
+++ src/data/fake.js
@@ -0,0 +1,87 @@
+module.exports = {
+
+  payment: [
+    {
+      item  : 'Hotově na prodejně',
+      price : '0,-',
+      note  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, nemo.'
+    },
+    {
+      item  : 'Dobírka',
+      price : '30,-',
+      note  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, nemo.'
+    },
+    {
+      item  : 'Bankovní převod',
+      price : '0,-',
+      note  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, nemo.'
+    },
+    {
+      item  : 'Platební karta',
+      price : '0,-',
+      note  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, nemo.'
+    },
+    {
+      item  : 'Platební brána Comgate',
+      price : '0,-',
+      note  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, nemo.'
+    },
+    {
+      item  : 'Platební brána Gopay',
+      price : '0,-',
+      note  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, nemo.'
+    }
+  ],
+
+  shipment: [
+    {
+      item  : 'Osobní odběr',
+      price : '0,-',
+      note  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, nemo.'
+    },
+    {
+      item  : 'Česká pošta',
+      price : '99,-',
+      note  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, nemo.'
+    },
+    {
+      item  : 'DPD',
+      price : '119,-',
+      note  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, nemo.'
+    },
+    {
+      item  : 'DHL',
+      price : '119,-',
+      note  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, nemo.'
+    },
+    {
+      item  : 'Lokální kurýr',
+      price : '89,-',
+      note  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, nemo.'
+    },
+    {
+      item  : 'Zásilkovna',
+      price : '69,-',
+      note  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, nemo.'
+    }
+  ],
+
+  product: [
+    'Enderova hra',
+    'Mluvčí za mrtvé',
+    'Xenocida',
+    'Děti ducha',
+    'Enderův stín',
+    'Hegemonův stín',
+    'Stínové loutky',
+    'Marťanská kronika',
+    '451 stupňů Fahrenheita',
+    'Kaleidoskop',
+    'Duna',
+    'Den trifidů',
+    'Dveře do léta',
+    'Cizinec v cizí zemi',
+    
+  ]
+
+};
